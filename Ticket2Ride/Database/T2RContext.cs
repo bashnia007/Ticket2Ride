c616065336df09df932d10206ebd216941c44fa3
aleksandr <alexanderkavokin@gmail.com>
2018-03-19 20:54:37 +0100

    Added EF6

---
+++ Ticket2Ride/Database/T2RContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket2Ride.Database
+{
+    public class T2RContext : DbContext
+    {
+        public T2RContext() : base("T2R_test_db")
+        {
+            
+        }
+
+        public DbSet<Connection> Connections { get; set; }
+    }
+}
