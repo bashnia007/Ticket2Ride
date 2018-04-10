816d839c5bef6702d4cbfe9f08bc116ad0074e8a
aleksandr <alexanderkavokin@gmail.com>
2018-04-10 22:57:34 +0200

    Added UI project

---
+++ Unity_Project/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unity_Project
+{
+    public class Class1
+    {
+    }
+}
